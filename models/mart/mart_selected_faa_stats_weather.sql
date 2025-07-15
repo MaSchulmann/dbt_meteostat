c73f91ff7f4e1d08202908ee9b7080b6095630fe
Maria Schulmann <mariaschulmann@ad92bb61-739a-49f2-9971-f98cdbe8586d.fritz.box>
2025-07-15 10:37:14 +0200

    Correcting models which are not running

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,10 +1,10 @@
 WITH weekly_weather AS (
     SELECT station_id AS faa
          , DATE_TRUNC('week', date) AS week
-         , ROUND(AVG(temp_min), 2) AS avg_temp_min
-         , ROUND(AVG(temp_max), 2) AS avg_temp_max
-         , SUM(precipitation) AS total_precipitation
-         , SUM(snowfall) AS total_snowfall
+         , ROUND(AVG(min_temp_c), 2) AS avg_min_temp_c
+         , ROUND(AVG(max_temp_c), 2) AS avg_max_temp_c
+         , SUM(precipitation_mm) AS total_precipitation_mm
+         , MAX(max_snow_mm) AS max_snow_mm
          , ROUND(AVG(wind_dir_avg), 2) AS avg_wind_dir
          , ROUND(AVG(wind_speed_avg), 2) AS avg_wind_speed
          , MAX(wind_peakgust) AS max_wind_gust
--- models/mart/mart_weather_weekly.sql
+++ models/mart/mart_weather_weekly.sql
@@ -1,29 +1,20 @@
 WITH weekly_aggregates AS (
-    SELECT faa
+    SELECT airport_code
          , DATE_TRUNC('week', date) AS week
 
-         -- Temperature: use min and max of daily values
-         , MIN(temp_min) AS min_temp_week
-         , MAX(temp_max) AS max_temp_week
-         , ROUND(AVG(temp_min), 2) AS avg_temp_min
-         , ROUND(AVG(temp_max), 2) AS avg_temp_max
+         -- Temperatures
+         , MIN(min_temp_c) AS min_temp_c_week
+         , MAX(max_temp_c) AS max_temp_c_week
+         , ROUND(AVG(avg_temp_c), 2) AS avg_temp_c_week
 
-         -- Precipitation/snow: add it up
-         , SUM(precipitation) AS total_precipitation
-         , SUM(snowfall) AS total_snowfall
-
-         -- Wind: use average and max
-         , ROUND(AVG(wind_dir_avg), 2) AS avg_wind_dir
-         , ROUND(AVG(wind_speed_avg), 2) AS avg_wind_speed
-         , MAX(wind_peakgust) AS max_wind_gust
-
-         -- Optional: mode of weekday or condition (requires custom aggregate)
-         -- , MODE() WITHIN GROUP (ORDER BY weekday) AS most_common_weekday
+         -- Precipitation and snow
+         , SUM(precipitation_mm) AS total_precipitation_mm
+         , MAX(max_snow_mm) AS max_snow_mm_week
 
     FROM {{ ref('prep_weather_daily') }}
-    GROUP BY faa, DATE_TRUNC('week', date)
+    GROUP BY airport_code, DATE_TRUNC('week', date)
 )
 
 SELECT *
 FROM weekly_aggregates
-ORDER BY week, faa
+ORDER BY week, airport_code
