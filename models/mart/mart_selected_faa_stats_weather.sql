987a249563ddc52d8c270ae1dc2f023a84f7d30a
Maria Schulmann <mariaschulmann@ad92bb61-739a-49f2-9971-f98cdbe8586d.fritz.box>
2025-07-15 10:48:53 +0200

    Correcting mart_selected_faa_stats_weather

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,27 +1,38 @@
 WITH weekly_weather AS (
-    SELECT station_id AS faa
-         , DATE_TRUNC('week', date) AS week
-         , ROUND(AVG(min_temp_c), 2) AS avg_min_temp_c
-         , ROUND(AVG(max_temp_c), 2) AS avg_max_temp_c
-         , SUM(precipitation_mm) AS total_precipitation_mm
-         , MAX(max_snow_mm) AS max_snow_mm
-         , ROUND(AVG(wind_dir_avg), 2) AS avg_wind_dir
-         , ROUND(AVG(wind_speed_avg), 2) AS avg_wind_speed
-         , MAX(wind_peakgust) AS max_wind_gust
-    FROM {{ ref('staging_weather_daily') }}
-    GROUP BY station_id, DATE_TRUNC('week', date)
-),
+    SELECT
+        airport_code,
+        DATE_TRUNC('week', date) AS week,
 
-airport_info AS (
-    SELECT faa, name, city, country
-    FROM {{ ref('staging_airports') }}
+        -- Temperatures
+        ROUND(AVG(avg_temp_c), 2) AS avg_temp_c_week,
+        MIN(min_temp_c) AS min_temp_c_week,
+        MAX(max_temp_c) AS max_temp_c_week,
+
+        -- Precipitation & Snow
+        SUM(precipitation_mm) AS total_precipitation_mm,
+        MAX(max_snow_mm) AS max_snow_mm_week,
+
+        -- Wind & Pressure
+        ROUND(AVG(avg_wind_direction), 1) AS avg_wind_direction_week,
+        ROUND(AVG(avg_wind_speed_kmh), 1) AS avg_wind_speed_kmh_week,
+        MAX(wind_peakgust_kmh) AS max_wind_peakgust_kmh_week,
+        ROUND(AVG(avg_pressure_hpa), 1) AS avg_pressure_hpa_week,
+
+        -- Sun
+        SUM(sun_minutes) AS total_sun_minutes_week,
+
+        -- Date Features (Mode)
+        MIN(date_year) AS year,  -- year of the week
+        MIN(cw) AS cw,           -- week number
+        MODE() WITHIN GROUP (ORDER BY month_name) AS most_common_month_name,
+        MODE() WITHIN GROUP (ORDER BY season) AS most_common_season
+    FROM {{ ref('prep_weather_daily') }}
+    GROUP BY airport_code, DATE_TRUNC('week', date)
 )
 
-SELECT w.*
-     , a.name
-     , a.city
-     , a.country
-FROM weekly_weather w
-LEFT JOIN airport_info a
-  ON w.faa = a.faa
-ORDER BY week, w.faa
+SELECT *
+FROM weekly_weather
+ORDER BY week, airport_code
+
+
+
